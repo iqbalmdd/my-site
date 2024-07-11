6b4084185d2fcf54c2be7aacf393983c8d10e867
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-11 17:39:01 +0700

    Nav Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
   return (
     <>
       <div className="fixed bottom-0 w-full flex justify-center z-50">
-        <div className="m-10 flex items-center justify-evenly rounded-full w-auto h-auto bg-gradient-to-tr from-purple-400 via-pink-500 to-red-500 p-[1.5px]">
-          <div className="bg-gray-900 h-full w-full rounded-full flex justify-center items-center">
+        <div className="m-10 flex items-center justify-evenly rounded-full w-auto h-auto bg-gradient-to-tr from-purple-400 via-pink-500 to-red-500 p-[1.5px] hover:scale-125 transition-all duration-100 ease-in-out">
+          <div className="bg-gray-900 h-full w-full rounded-full flex justify-center items-center hover:scale-125 transition-all duration-100 ease-in-out">
             <button className="m-2 hover:bg-gradient-to-tr from-purple-400 via-pink-500 to-red-500 bg-opacity-50 h-8 w-8 rounded-full transition-all duration-100 ease-in-out ">
             <GoPerson className="w-full z-10 hover:scale-125 transition-all duration-100 ease-in-out"/>
             </button>
